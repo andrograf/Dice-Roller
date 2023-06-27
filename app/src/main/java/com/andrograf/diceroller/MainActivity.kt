7cc2091ab188dd96cdf8f0b22d4c1d1de9ce2970
Patricia P <andrograf31@gmail.com>
2023-06-27 11:49:33 +0200

    feat: change preview setting

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -37,7 +37,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     DiceRollerTheme {
