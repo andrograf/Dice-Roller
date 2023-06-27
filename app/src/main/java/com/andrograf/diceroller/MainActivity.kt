1cc69f519138368384b9421a704c8a3bb6c06b48
Patricia P <andrograf31@gmail.com>
2023-06-27 12:56:32 +0200

    feat: create delegate from result variable

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result = 1
+    var result by remember { mutableStateOf(1) }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
