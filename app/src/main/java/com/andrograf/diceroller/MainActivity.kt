870a48df0322af6851cdde46fc49ab470e39df8a
Patricia P <andrograf31@gmail.com>
2023-06-27 12:51:20 +0200

    feat: add random generator lambda expression

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result = 1
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -46,7 +47,9 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             contentDescription = "One"
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = { result = (1..6).random() }
+        ) {
             Text(stringResource(R.string.roll))
         }
     }
