ed56937cb57949141a44924485b9684b0acd48aa
Patricia P <andrograf31@gmail.com>
2023-06-27 12:32:38 +0200

    feat: add button composable

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.andrograf.diceroller.ui.theme.DiceRollerTheme
 
@@ -33,7 +36,9 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
     }
 }
 
