fdec92ddb79cec8443b1b45f612303da7c3970c5
Patricia P <andrograf31@gmail.com>
2023-06-27 12:40:04 +0200

    feat: add image composable to layout

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andrograf.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.andrograf.diceroller.ui.theme.DiceRollerTheme
@@ -36,6 +38,10 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "One"
+        )
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
