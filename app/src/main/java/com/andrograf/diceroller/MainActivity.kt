bf1ca7b121a6a895d7e0adb391046cd38e0b8bc1
Patricia P <andrograf31@gmail.com>
2023-06-27 12:07:59 +0200

    feat: remove Surface parameters

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -17,10 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface {
                     DiceRollerApp()
                 }
             }
