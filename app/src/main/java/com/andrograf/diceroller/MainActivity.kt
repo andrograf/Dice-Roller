d65bbf772c015d93f22bb6bc245522c17f11ed46
Patricia P <andrograf31@gmail.com>
2023-06-27 11:57:03 +0200

    refactor: rename function

--- app/src/main/java/com/andrograf/diceroller/MainActivity.kt
+++ app/src/main/java/com/andrograf/diceroller/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DiceRoller()
+                    DiceRollerApp()
                 }
             }
         }
@@ -38,7 +38,7 @@ fun DiceWithButtonAndImage() {
     showSystemUi = true
 )
 @Composable
-fun DiceRoller() {
+fun DiceRollerApp() {
 
 }
 
