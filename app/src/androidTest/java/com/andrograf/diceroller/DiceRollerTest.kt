67ef7c27d5e43e18d51b85b07ae17594800bca0b
Patricia P <andrograf31@gmail.com>
2023-06-28 11:53:38 +0200

    test: create instrumented test class

---
+++ app/src/androidTest/java/com/andrograf/diceroller/DiceRollerTest.kt
@@ -0,0 +1,9 @@
+package com.andrograf.diceroller
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class DiceRollerTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
