8331f7a201a4880b05c79f2c9e62cad9712a7737
Patricia P <andrograf31@gmail.com>
2023-06-28 12:10:28 +0200

    test: create test

--- app/src/androidTest/java/com/andrograf/diceroller/DiceRollerTest.kt
+++ app/src/androidTest/java/com/andrograf/diceroller/DiceRollerTest.kt
@@ -2,8 +2,17 @@ package com.andrograf.diceroller
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Rule
+import org.junit.Test
 
 class DiceRollerTest {
     @get:Rule
     val composeTestRule = createComposeRule()
+
+    @Test
+    fun diceWithButtonAndImageTest(){
+        composeTestRule.setContent {
+            DiceWithButtonAndImage()
+        }
+
+    }
 }
